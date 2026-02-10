7c7462f12e0aedd11a501750c4656526e17bd958
ysf-ad <yousifaldakoki@gmail.com>
2026-02-10 00:33:00 -0500

    fixed woocommerce endpoint, unified data object TODO

--- src/services/scraper/strategies/woocommerce.ts
+++ src/services/scraper/strategies/woocommerce.ts
@@ -6,7 +6,7 @@ export const WooCommerceStrategy: ScraperStrategy = {
     name: 'WooCommerce',
     description: 'Extracts data from WooCommerce via WP-JSON API',
     match: async (url) => {
-        const response = await httpClient.get(url + "/wp-json/wc/v3/products");
+        const response = await httpClient.get(url + "/wp-json/wc/store/v1/products");
         if (response.ok) {
             return {
                 isMatch: true,
@@ -20,7 +20,7 @@ export const WooCommerceStrategy: ScraperStrategy = {
         };
     },
     scrape: async (req: ScraperRequest) => {
-        const response = await httpClient.get(req.url + "/wp-json/wc/v3/products");
+        const response = await httpClient.get(req.url + "/wp-json/wc/store/v1/products");
         return response.json();
     }
 };
