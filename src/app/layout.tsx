6f7fd7a6b366620ecce1e1b5ace64dafbc69881f
Abdelrahman Eissa <bedoeissa0@gmail.com>
2026-02-13 22:21:00 -0500

    Removed line from header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-              <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4">
+              <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <AuthButton />
               </header>
